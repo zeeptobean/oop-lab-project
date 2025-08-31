fe201824be52a62676a8a914a2683b9650161509
zeeptobean <zeeptobean@gmail.com>
2025-08-31 18:24:38 +0700

    book getTagsString tweak

--- src/core/book.cpp
+++ src/core/book.cpp
@@ -15,9 +15,7 @@ std::string Book::getTagsString() const {
     std::string result;
     for(size_t i = 0; i < tags.size(); i++) {
         result += tags[i];
-        if(i != tags.size() - 1) {
-            result += ", ";
-        }
+        result += ", ";
     }
     result += getAuthorsString();
     return result;
